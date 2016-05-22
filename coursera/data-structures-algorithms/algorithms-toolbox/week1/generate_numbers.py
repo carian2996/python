7d268a8654e185e845f77da8a002f290f432c10c
Ian Castillo Rosales <carian2996@gmail.com>
2016-05-22 12:49:48 -0500

    Use seed and assert
    Use a seed to reproduce the test and assertions for constrains of the
    problem.

--- coursera/data-structures-algorithms/algorithms-toolbox/week1/generate_numbers.py
+++ coursera/data-structures-algorithms/algorithms-toolbox/week1/generate_numbers.py
@@ -1,9 +1,21 @@
 # python3
 
-import numpy as np
+from random import randint
+from random import seed
 
+# print("How many elements do you want to generate?")
 n = int(input())
-v = np.random.choice(5, n)
+assert(n >= 2), "You should generate at least 2 elements"
+
+# print("Choose the lowest value in the range of the array: ")
+v_min = int(input())
+assert(v_min >= 0), "You can't use negative numbers"
+
+# print("Choose the highest value in the range of the array: ")
+v_max = int(input())
+assert(v_max <= 100000), "The numbers can't be greater than 100000"
+
+v = [randint(v_min, v_max) for p in range(0, n)]
 
 print(n)
 print(*v)
