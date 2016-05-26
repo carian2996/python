62bc0c09e22acf56e6f88108b5ad871201398c63
Ian Castillo Rosales <carian2996@gmail.com>
2016-05-26 17:38:27 -0500

    Least Common Multiple.
    Given two integers a and b, find their least common multiple.

--- coursera/data-structures-algorithms/algorithms-toolbox/week2/lcm/lcm.py
+++ coursera/data-structures-algorithms/algorithms-toolbox/week2/lcm/lcm.py
@@ -1,12 +1,16 @@
 # Uses python3
 import sys
 
+def gcd(a, b):
+	if b == 0: return a
+	rem_a = a % b
+
+	return gcd(b, rem_a)
+
 def lcm(a, b):
-    #write your code here
-    return a*b
+    return int((a*b)/gcd(a, b))
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
+    a, b = map(int, input().split())
     print(lcm(a, b))
 
