3ec6a66d22e62041e4900ea44ad4ab6cdbcf9dce
Ian Castillo Rosales <carian2996@gmail.com>
2016-05-29 18:54:32 -0500

    Force integer division

--- coursera/data-structures-algorithms/algorithms-toolbox/week2/lcm/lcm.py
+++ coursera/data-structures-algorithms/algorithms-toolbox/week2/lcm/lcm.py
@@ -8,9 +8,12 @@ def gcd(a, b):
 	return gcd(b, rem_a)
 
 def lcm(a, b):
-    return int((a*b)/gcd(a, b))
+	assert(1 <= a and 1 <= b), "The number should be greater than 1"
+	assert(a <= 2000000000 and b <= 2000000000), "The number should be less than 2x10^9"
+
+	return int( (a*b)//gcd(a, b) )
+
 
 if __name__ == '__main__':
     a, b = map(int, input().split())
     print(lcm(a, b))
-
